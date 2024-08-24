7efcf35f40447c11563fb42f43a4cb87f7553552
Alina Mishchenko <153620209+AlinaMishchenk@users.noreply.github.com>
2024-08-24 20:15:14 +0200

    Create ft_lstclear_bonus.c

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almishch <almishch@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 15:06:23 by almishch          #+#    #+#             */
+/*   Updated: 2024/08/24 20:06:52 by almishch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)-> next;
+		del ((*lst)-> content);
+		free(*lst);
+		*lst = temp;
+	}
+	*lst = NULL;
+}
