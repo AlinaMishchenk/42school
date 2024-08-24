257d29de4ded98ea3e4c9ae413cba235ccbdffdd
Alina Mishchenko <153620209+AlinaMishchenk@users.noreply.github.com>
2024-08-24 20:32:25 +0200

    Create ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almishch <almishch@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 19:15:09 by almishch          #+#    #+#             */
+/*   Updated: 2024/08/20 15:43:01 by almishch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	void	*dest_ptr;
+
+	dest_ptr = dest;
+	if (!dest && !src)
+		return (dest);
+	if (dest == src)
+		return (dest);
+	if (dest > src)
+	{
+		while (n--)
+			((char *)dest)[n] = ((char *)src)[n];
+	}
+	else
+	{
+		while (n--)
+			*(char *)dest++ = *(char *)src++;
+	}
+	return (dest_ptr);
+}
