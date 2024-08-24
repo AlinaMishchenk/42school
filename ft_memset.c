c4e0cff64269b29583fe74ddb4d21e6885bc7456
Alina Mishchenko <153620209+AlinaMishchenk@users.noreply.github.com>
2024-08-24 20:32:44 +0200

    Create ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almishch <almishch@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 19:20:18 by almishch          #+#    #+#             */
+/*   Updated: 2024/08/20 15:43:15 by almishch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *str, int c, size_t n)
+{
+	void	*str_ptr;
+
+	str_ptr = str;
+	while (n--)
+		*(unsigned char *)str++ = (unsigned char)c;
+	return (str_ptr);
+}
