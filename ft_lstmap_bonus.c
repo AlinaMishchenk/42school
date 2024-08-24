0acb86afd6b43da59809b89a0ea68e6ffbaaf6ba
Alina Mishchenko <153620209+AlinaMishchenk@users.noreply.github.com>
2024-08-24 20:19:15 +0200

    Create ft_lstmap_bonus.c

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almishch <almishch@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 20:18:18 by almishch          #+#    #+#             */
+/*   Updated: 2024/08/24 20:18:49 by almishch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Sozdanie novogo spiska, gde kazdiy element preobrazovan s pomoshiu
+//function, peredannoy v argumente. Pri etom neobhodimo osvobodit pamiat
+//za elementi starogo spiska s pomoshiu function del.
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_obj;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		new_obj = ft_lstnew(f(lst->content));
+		if (!new_obj)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_obj);
+		lst = lst->next;
+	}
+	return (new_list);
+}
