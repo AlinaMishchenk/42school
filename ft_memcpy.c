978733f3243de2fb0d52c3878501151a6fad765b
Alina Mishchenko <153620209+AlinaMishchenk@users.noreply.github.com>
2024-08-24 20:31:53 +0200

    Create ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almishch <almishch@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 19:04:08 by almishch          #+#    #+#             */
+/*   Updated: 2024/08/20 15:42:49 by almishch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, void const *src, size_t n)
+{
+	void	*dest_ptr;
+
+	dest_ptr = dest;
+	if (!dest && !src)
+		return (dest);
+	while (n--)
+		*(char *)dest++ = *(char *)src++;
+	return (dest_ptr);
+}
