c5615475953a008ccf9a85a0feeab9fd623f81a7
Alina Mishchenko <153620209+AlinaMishchenk@users.noreply.github.com>
2024-08-24 20:55:00 +0200

    Create ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almishch <almishch@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 14:18:08 by almishch          #+#    #+#             */
+/*   Updated: 2024/08/08 14:21:41 by almishch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(char const *haystack, char const *needle, size_t n)
+{
+	size_t	j;
+
+	if (!haystack && !n)
+		return (NULL);
+	if (!*needle)
+		return ((char *)haystack);
+	while (*haystack && n--)
+	{
+		j = 0;
+		while (*(haystack + j) == *(needle + j)
+			&& *(needle + j) && j <= n)
+		{
+			if (!*(needle + j + 1))
+				return ((char *)haystack);
+			j++;
+		}
+		haystack++;
+	}
+	return (NULL);
+}
